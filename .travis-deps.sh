bcdce428d9fb375fa9e418b5d57fc22da211a11e
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2015-10-20 09:28:33 +0200

    Test10

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -12,10 +12,11 @@ cd ctrulib/libctru && make ; cd -
 sudo cp -rf ctrulib/libctru/ ${DEVKITPRO}
 
 # Get, Build & Install dependencies
+export DEVKITARM=${DEVKITPRO}/devkitARM
 
 # portlibs
-# git clone https://github.com/xerpi/3ds_portlibs.git
-# cd 3ds_portlibs && sudo make all ; cd -
+git clone https://github.com/xerpi/3ds_portlibs.git
+cd 3ds_portlibs && sudo make all ; cd -
 
 # sf2dlib
 git clone https://github.com/xerpi/sf2dlib.git
